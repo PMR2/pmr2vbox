fa4ddc81288cf0d185e151447f2310cc8127b809
Tommy Yu <tommy.yu@auckland.ac.nz>
2019-11-26 18:40:41 +1300

    Rework the docker service setup
    - Ensure that postgresql is started after docker so that postgresql can
      listen on the bridge device.
    - Also ensure the VM is restarted after rebuilding the kernel.

--- gentoo/server/install_physiome-coko.sh
+++ gentoo/server/install_physiome-coko.sh
@@ -19,6 +19,29 @@ emerge --noreplace \
     www-servers/apache \
     dev-db/postgresql:${POSTGRESQL_VERSION} \
 
+# TODO check each thing individually and not make assumptions for the
+# kernel options required for docker
+if ! zcat /proc/config.gz | grep -P "CONFIG_CGROUP_NET_PRIO=(m|y)" > /dev/null 2>&1; then
+    echo "Recompiling kernel with options required for docker"
+    echo "The VM will be restarted when compilation is completed"
+    cat >> /usr/src/linux/.config <<- EOF
+	CONFIG_IP_NF_TARGET_MASQUERADE=m
+	CONFIG_IP_NF_NAT=m
+	CONFIG_CGROUP_PIDS=y
+	CONFIG_CGROUP_HUGETLB=y
+	CONFIG_NET_L3_MASTER_DEV=y
+	CONFIG_IPVLAN=m
+	CONFIG_CGROUP_NET_PRIO=y
+	CONFIG_OVERLAY_FS=m
+	CONFIG_NETFILTER_ADVANCED=y
+	CONFIG_IP_NF_TARGET_REDIRECT=m
+	EOF
+    genkernel --oldconfig --no-zfs --no-btrfs all
+    echo "Restarting now - please rerun script again after VM is fully booted"
+    shutdown -r now
+    exit 1
+fi
+
 ### Apache
 
 sed -i 's/^APACHE2_OPTS.*/APACHE2_OPTS="-D DEFAULT_VHOST -D INFO -D SSL -D SSL_DEFAULT_VHOST -D LANGUAGE -D PROXY"/' /etc/conf.d/apache2
@@ -52,35 +75,37 @@ EOF
 
 npm install node-gyp -g
 
-# TODO check each thing individually and not make assumptions for the
-# kernel options required for docker
-if ! zcat /proc/config.gz | grep -P "CONFIG_IP_NF_NAT=(m|y)" > /dev/null 2>&1; then
-    cat >> /usr/src/linux/.config <<- EOF
-	CONFIG_IP_NF_TARGET_MASQUERADE=m
-	CONFIG_IP_NF_NAT=m
-	CONFIG_CGROUP_PIDS=y
-	CONFIG_CGROUP_HUGETLB=y
-	CONFIG_NET_L3_MASTER_DEV=y
-	CONFIG_IPVLAN=m
-	CONFIG_CGROUP_NET_PRIO=y
-	CONFIG_OVERLAY_FS=m
-	CONFIG_NETFILTER_ADVANCED=y
-	CONFIG_IP_NF_TARGET_REDIRECT=m
-	EOF
-    genkernel --oldconfig --no-zfs --no-btrfs all
-fi
-
 if ! id -u ${PHYSIOME_USER} > /dev/null 2>&1; then
     useradd -m -k /etc/skel/ ${PHYSIOME_USER}
 fi
+
+# Start up docker first as this will take some time
+/etc/init.d/docker start
+rc-update add docker default
+echo "waiting for docker to fully start"
+sleep 5
+DOCKER_HOST=$(docker network inspect bridge --format '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
 
 POSTGRES_DATA_DIR=/var/lib/postgresql/${POSTGRESQL_VERSION}/data
 if [ ! -d "${POSTGRES_DATA_DIR}" ]; then
     emerge --config dev-db/postgresql:${POSTGRESQL_VERSION}
 
+    # Ensure postgresql listen on the docker bridge
+    cat >> ${POSTGRES_DATA_DIR}/postgresql.conf <<- EOF
+	listen_addresses = '127.0.0.1, ${DOCKER_HOST}'
+	EOF
+    cat >> ${POSTGRES_DATA_DIR}/pg_hba.conf <<- EOF
+	host    all             all             ${DOCKER_HOST}/24            trust
+	EOF
+
+    # This is a workaround to ensure that the docker network bridge
+    # network device is started, so that postgresql will be able to
+    # start listening on it.
+    sed -i 's/use net$/use net docker/' \
+        /etc/init.d/postgresql-${POSTGRESQL_VERSION}
+
     /etc/init.d/postgresql-${POSTGRESQL_VERSION} start
     rc-update add postgresql-${POSTGRESQL_VERSION} default
-
 else
     echo found "${POSTGRES_DATA_DIR}", assume postgres is configured.
 fi
@@ -89,7 +114,9 @@ fi
 # failure is non-fatal so simply ignore that...
 psql -U postgres -d template1 << EOF
     CREATE USER physiome CREATEDB;
+    ALTER USER physiome WITH PASSWORD 'physiome';
     CREATE DATABASE physiome WITH OWNER=physiome;
+    CREATE DATABASE physiome_camunda WITH OWNER=physiome;
 EOF
 
 psql -U postgres -d physiome << EOF
@@ -99,9 +126,15 @@ EOF
 
 # TODO figure out how to ensure that this instance starts when the
 # physiome-coko starts during runtime.
-/etc/init.d/docker start
-rc-update add docker default
-docker run -d --name physiome-camunda -p 8080:8080 camunda/camunda-bpm-platform:latest
+
+docker run -d --name physiome-camunda -p 8080:8080 \
+    --restart always \
+    -e DB_DRIVER=org.postgresql.Driver \
+    -e DB_URL=jdbc:postgresql://${DOCKER_HOST}:5432/physiome_camunda \
+    -e DB_USERNAME=physiome \
+    -e DB_PASSWORD=physiome \
+    -e WAIT_FOR=${DOCKER_HOST}:5432 \
+    camunda/camunda-bpm-platform:latest
 
 # TODO switch to physiome user
 su - ${PHYSIOME_USER} -c "/bin/bash" << EOF
